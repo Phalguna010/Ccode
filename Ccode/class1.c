abbaae98b4f6503d2f63fd03fcb67b830d62787c
Phalguna010 <phalguna20ucse120@mahindrauniversity.edu.in>
2021-02-25 13:13:33 +0530

    first commit

---
+++ Ccode/class1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+
+
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    int i = 0;
+    
+
+    while(i<n){
+        printf("hi");
+        i++;
+    }
+
+
+}
